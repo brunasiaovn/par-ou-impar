102783b1d39b55a7e23e9c2e4e5e5994bbf19b71
Bruna <139784873+brunasiaovn@users.noreply.github.com>
2024-01-15 12:23:18 -0300

    Update parimar.js

--- parimar.js
+++ parimar.js
@@ -4,9 +4,8 @@ function teste() {
     if (num % 2 == 0) {
         tipo = '<strong>PAR</strong>'
     } else {
-        tipo = '<strong>ÍMPAR</strong>'
+        tipo = '<strong>IMPAR</strong>'
     }
-
     let res = document.querySelector('section#result')
-    res.innerHTML = `<p>O número ${num} que foi digitado é ${tipo}!</p>`
+    res.innerHTML = `<p>O número <mark>${num}</mark> que foi digitado é ${tipo}</p>`
 }
